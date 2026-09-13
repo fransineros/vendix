067826f77babbf07bfc525cc7317f3eac072b462
fransineros <55166256+fransineros@users.noreply.github.com>
2026-09-13 20:16:11 +0200

    Delete apps/web/tailwind.config_2.js

--- apps/web/tailwind.config_2.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: { vendix: { 500: '#7c3aed', 600: '#6d28d9' } }
-    }
-  },
-  plugins: []
-}
