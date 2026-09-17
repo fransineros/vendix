593ad3d2b1d64aa64fcebddc4f21959f73643bfb
fransineros <55166256+fransineros@users.noreply.github.com>
2026-09-18 00:36:54 +0200

    fix next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,5 +2,12 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
+  experimental: {
+    esmExternals: 'loose',
+  },
 }
 module.exports = nextConfig
