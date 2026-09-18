43553c2fe8408b03cbcb269b0e32d4dbe92deb15
fransineros <55166256+fransineros@users.noreply.github.com>
2026-09-18 13:00:37 +0200

    fix: minimal working next.config - only onnxruntime-node + sharp

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,24 +1,11 @@
-const webpack = require('webpack');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  transpilePackages: [],
-  webpack: (config, { isServer }) => {
-    config.resolve.fallback = { ...config.resolve.fallback, fs: false, path: false, module: false, crypto: false, stream: false, os: false };
-    config.plugins = [
-      ...config.plugins,
-      new webpack.IgnorePlugin({ resourceRegExp: /^onnxruntime-node$/ }),
-      new webpack.IgnorePlugin({ resourceRegExp: /^@imgly\/background-removal/ }),
-      new webpack.IgnorePlugin({ resourceRegExp: /^sharp$/ }),
-    ];
-    config.module.exprContextCritical = false;
-    config.module.rules.push({ test: /\.map$/, use: [] });
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false, os: false };
     return config;
   },
   experimental: {
-    esmExternals: 'loose',
-    serverComponentsExternalPackages: ['onnxruntime-node', 'sharp', '@imgly/background-removal-node', '@imgly/background-removal', 'onnxruntime-web'],
+    serverComponentsExternalPackages: ['onnxruntime-node', 'sharp'],
   },
 }
 module.exports = nextConfig
