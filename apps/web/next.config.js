063d4b1ac9fce656ef0a95a4241dc4576c353eb0
fransineros <55166256+fransineros@users.noreply.github.com>
2026-09-19 02:09:16 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,23 +3,22 @@ const webpack = require('webpack');
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  transpilePackages: [],
   webpack: (config, { isServer }) => {
-    config.resolve.fallback = { ...config.resolve.fallback, fs: false, path: false };
-    if (isServer) {
-      config.plugins = [
-        ...config.plugins,
-        new webpack.IgnorePlugin({ resourceRegExp: /^@imgly\/background-removal$/ }),
-        new webpack.IgnorePlugin({ resourceRegExp: /^onnxruntime-node$/ }),
-        new webpack.IgnorePlugin({ resourceRegExp: /^sharp$/ }),
-      ];
-    }
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false, path: false, module: false, crypto: false, stream: false, os: false };
+    config.plugins = [
+      ...config.plugins,
+      new webpack.IgnorePlugin({ resourceRegExp: /^onnxruntime-node$/ }),
+      new webpack.IgnorePlugin({ resourceRegExp: /^@imgly\/background-removal/ }),
+      new webpack.IgnorePlugin({ resourceRegExp: /^sharp$/ }),
+    ];
     config.module.exprContextCritical = false;
     config.module.rules.push({ test: /\.map$/, use: [] });
     return config;
   },
   experimental: {
     esmExternals: 'loose',
-    serverComponentsExternalPackages: ['onnxruntime-node', 'sharp'],
+    serverComponentsExternalPackages: ['onnxruntime-node', 'sharp', '@imgly/background-removal-node', '@imgly/background-removal', 'onnxruntime-web'],
   },
 }
 module.exports = nextConfig
